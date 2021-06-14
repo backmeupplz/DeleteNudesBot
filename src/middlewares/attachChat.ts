91b5d2f45d44fd8fb3a5779e1f64d5fa2583d438
backmeupplz <backmeupplz@gmail.com>
2021-06-14 13:58:01 -0700

    fix chat

--- src/middlewares/attachChat.ts
+++ src/middlewares/attachChat.ts
@@ -2,6 +2,6 @@ import { findChat } from '@/models'
 import { Context } from 'telegraf'
 
 export async function attachChat(ctx: Context, next: () => void) {
-  ctx.dbchat = await findChat(ctx.from.id)
+  ctx.dbchat = await findChat(ctx.chat.id)
   return next()
 }
